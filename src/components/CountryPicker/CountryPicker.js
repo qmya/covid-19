f04b5097219635099a3d0c32f878a08b7c9a5b80
qmya <qandeelgul90@gmail.com>
2020-08-28 17:55:26 -0400

    added style for different screens

--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import {NativeSelect, FormControl, StylesProvider } from '@material-ui/core';
+import {NativeSelect, FormControl } from '@material-ui/core';
 import styles from './CountryPicker.module.css';
 import { fetchCountries } from '../../api';
 
 const CountryPicker =({ handleCountryChange }) => {
 const [fetchedCountries, setFetchedCountries] = useState([]);
+
     useEffect(() => {
         const fetchAPI = async () => {
             setFetchedCountries(await fetchCountries());
@@ -14,10 +15,10 @@ const [fetchedCountries, setFetchedCountries] = useState([]);
 
     console.log(fetchCountries)
     return (
-        <FormControl className= {styles.FormControl}>
+        <FormControl className= {styles.formControl}>
             <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
                 <option value="global"> Global </option>
-    {fetchedCountries.map(( country, i ) => <option key={i} value= {country}>{country}</option>)}
+                {fetchedCountries.map(( country, i ) => <option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
     )
