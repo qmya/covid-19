21671ba593766b0e2e4e20c42a594924cda7a70b
qmya <qandeelgul90@gmail.com>
2020-08-30 00:14:15 -0400

    cleanup and prettier the code

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -3,7 +3,7 @@ import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 import styles from './Charts.module.css';
 
-const Charts =({ data:{ confirmed, deaths, recovered } }, country ) => {
+const Charts = ({ data: { confirmed, recovered, deaths }, country }) => {
     const [ dailyData, setDailyData ] = useState([]);
 
     useEffect(() => {
@@ -13,11 +13,33 @@ const Charts =({ data:{ confirmed, deaths, recovered } }, country ) => {
          
         fetchAPI();
     }, []);
+      //line chart
+      const lineChart = (
+        dailyData.length !== 0
+        ?(
+        <Line
+        data={{
+            labels: dailyData.map(({ date }) => date),
+            datasets: [{
+                data: dailyData.map(({ confirmed }) => confirmed ),
+                label: 'Infected',
+                borderColor: '#3333ff',
+                fill: true,
+            }, {
+                data: dailyData.map(({ deaths }) => deaths ),
+                label: 'Deaths',
+                borderColor: 'red',
+                backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                fill: true,
+            },
+        ],
+        }}
+        />):null
+    );
 
     //bar chart
     const barChart = (
-        confirmed
-        ? (
+        confirmed ? (
             <Bar
              data={{
                 labels: ['Infected', 'Recovered', 'Deaths'],
@@ -42,36 +64,15 @@ const Charts =({ data:{ confirmed, deaths, recovered } }, country ) => {
         ) : null
     );
 
-    //line chart
-    const lineChart = (
-        dailyData.length !== 0
-        ?(
-        <Line
-        data={{
-            labels: dailyData.map(({ date }) => date ),
-            datasets: [{
-                data: dailyData.map(({ confirmed }) => confirmed ),
-                label: 'Infected',
-                borderColor: '#3333ff',
-                fill: true,
-            }, {
-                data: dailyData.map(({ deaths }) => deaths ),
-                label: 'Deaths',
-                borderColor: 'red',
-                backgroundColor: 'rgba(255, 0, 0, 0.5)',
-                fill: true,
-            },
-        ],
-        }}
-        />):null
-    );
+  
 
  
     
     return (
         <div className={styles.container}>
             
-            {country ? barChart : lineChart }
+            { country ? lineChart : barChart }
+            console.log(lineChart)
             
         </div>
     )
